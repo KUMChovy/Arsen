5fe6e37738d69c3a77fd0e63a70ff41552846fad
isa <isai.c.miguel.twoinky@gmail.com>
2026-07-19 22:09:45 -0600

    feat: add offline pwa shell

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { AppProviders } from './app/providers'
+import { registerServiceWorker } from './app/registerServiceWorker'
 import { router } from './app/router'
 import './styles.css'
+
+registerServiceWorker()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
